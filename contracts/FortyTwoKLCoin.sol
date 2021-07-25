0859acd648fbd535d76e057a82b36b0c67403f53
Eason Chai <echai2905@gmail.com>
2021-07-25 15:50:45 +0800

    Add proper truffle config

--- contracts/FortyTwoKLCoin.sol
+++ contracts/FortyTwoKLCoin.sol
@@ -2,12 +2,23 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 /// @title ERC-20 Implementation of 42KL Coin
 /// @author Eason Chai
-/// @dev Basic ERC-20 Implementation
-contract FortyTwoKLCoin is ERC20 {
-    constructor() ERC20("42KL Coin", "42KL") {
+/// @dev Basic ERC-20 Implementation with RBAC
+contract FortyTwoKLCoin is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
+    constructor() ERC20("42KL Coin", "42KL") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function addMinter(address newMinter) public {
+        grantRole(MINTER_ROLE, newMinter);
     }
 }
--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
 /// @title Marketplace for 42KL Coin
 /// @author Eason Chai
 /// @dev This is where we can convert 42KL coin to eval points, etc.
 contract Marketplace {
-  constructor() public {
+  address ftklCoinAddress;
+  
+
+  constructor(address _coinAddress) {
+    ftklCoinAddress = _coinAddress;
   }
+  
 }

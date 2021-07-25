381b0fd8aceba6e67fca0cf2401153f84538c99b
Eason Chai <echai2905@gmail.com>
2021-07-25 14:45:11 +0800

    Add 42KL Coin Contract

---
+++ contracts/FortyTwoKLCoin.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @title ERC-20 Implementation of 42KL Coin
+/// @author Eason Chai
+/// @dev Basic ERC-20 Implementation
+contract FortyTwoKLCoin is ERC20 {
+    constructor() ERC20("42KL Coin", "42KL") {
+        
+    }
+}

bc548b0394f9b76c89350a3488d5dc0780992551
Eason Chai <echai2905@gmail.com>
2021-07-25 18:38:49 +0800

    Add migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const FortyTwoKLCoin = artifacts.require("FortyTwoKLCoin");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(FortyTwoKLCoin);
 };

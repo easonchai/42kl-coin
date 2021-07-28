8cb036492865164784f00259f60784a66cd5afed
Eason Chai <echai2905@gmail.com>
2021-07-28 17:24:40 +0800

    Init TS

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,13 @@
+// Retrieve environment variables
+require("dotenv").config();
+
 const Web3 = require("web3");
 const BN = require("bn.js");
 const axios = require("axios");
+const PRIVATE_KEY = process.env.PRIVATE_KEY || null;
 
 function init() {
-  console.log("Initializing backend");
+  console.log("📡 Initializing backend...");
 }
 
 init();
